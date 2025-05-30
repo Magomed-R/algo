6d3103d7c8a5923d75d423a68272740c20d5b120
magomed-r <magomed-radzhabov@internet.ru>
2025-05-30 14:23:06 +0300

    solve lowestCommonAncestor

--- algos/trees/lowestCommonAncestor.ts
+++ algos/trees/lowestCommonAncestor.ts
@@ -1,6 +1,14 @@
-import maxDepth from "./maxDepth";
-import TreeNode from "./entities/TreeNode";
+import TreeNode from './entities/TreeNode'
 
 export default function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-    
-};
+  if (!root) return null
+
+  if (root === p || root === q) return root
+  if (root.val > p!.val && root.val < q!.val) return root
+  if (root.val < p!.val && root.val > q!.val) return root
+
+  if (root.val > p!.val && root.val > q!.val) return lowestCommonAncestor(root.left, p, q)
+  if (root.val < p!.val && root.val < q!.val) return lowestCommonAncestor(root.right, p, q)
+
+  return null
+}
--- tests/trees/lowestCommonAncestor.test.ts
+++ tests/trees/lowestCommonAncestor.test.ts
@@ -1,15 +1,22 @@
-import { expect, test } from "@jest/globals";
-import TreeNode from "../../algos/trees/entities/TreeNode";
+import TreeNode from '../../algos/trees/entities/TreeNode'
 import lowestCommonAncestor from '../../algos/trees/lowestCommonAncestor'
 
-test('first', () => {
-    const tree = new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))), new TreeNode(8, new TreeNode(7), new TreeNode(9)))
+describe('lowestCommonAncestor', () => {
+  test('first', () => {
+    const p = new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5)))
+    const q = new TreeNode(8, new TreeNode(7), new TreeNode(9))
 
-    expect(lowestCommonAncestor(tree, new TreeNode(2), new TreeNode(8))).toBe(6)
-})
+    const bst = new TreeNode(6, p, q)
 
-test('second', () => {
-    const tree = new TreeNode(6, new TreeNode(2, new TreeNode(0), new TreeNode(4, new TreeNode(3), new TreeNode(5))), new TreeNode(8, new TreeNode(7), new TreeNode(9)))
+    expect(lowestCommonAncestor(bst, p, q)).toStrictEqual(bst)
+  })
 
-    expect(lowestCommonAncestor(tree, new TreeNode(2), new TreeNode(4))).toBe(2)
+  test('second', () => {
+    const q = new TreeNode(4, new TreeNode(3), new TreeNode(5))
+    const p = new TreeNode(2, new TreeNode(0), q)
+
+    const bst = new TreeNode(6, p, new TreeNode(8, new TreeNode(7), new TreeNode(9)))
+
+    expect(lowestCommonAncestor(bst, p, q)).toStrictEqual(p)
+  })
 })
